974aaa4c82084596b3e52b80e397402f64dde6d7
Cameron <campoore2@gmail.com>
2016-07-01 07:18:57 -0700

    Created initial files in the Word Count app. It will have a user input a word and a sentence which will return how many times a word appears in that sentence.

---
+++ spec/wordcount_spec.rb
@@ -0,0 +1,15 @@
+require("sinatra")
+require('capybara/rspec')
+require("wordcount.rb")
+
+describe("String#wordcount") do
+  it("")do
+    expect().to()
+  end
+  it("")do
+    expect().to()
+  end
+  it("")do
+    expect().to()
+  end
+end
