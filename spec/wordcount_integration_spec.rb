560c3d905dcd1c99a02f3864f2aeed0d1fea5545
Cameron Poore <campoore2@gmail.com>
2016-07-19 16:32:44 -0700

    Working integration spec.

--- spec/wordcount_integration_spec.rb
+++ spec/wordcount_integration_spec.rb
@@ -7,7 +7,7 @@ describe('word frequency count', {:type => :feature}) do
   it('takes a single word and determines how many times it appears in a given phrase') do
     visit('/')
     fill_in('word', :with => "pet")
-    fill_in('phrase', :with => "i have a pet dog and a pet cat")
+    fill_in('sentence', :with => "i have a pet dog and a pet cat")
     click_button('Submit')
     expect(page).to have_content("The word 'pet' shows up 2 times.")
   end
